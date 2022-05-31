c73adde8d3fc17dfb2beeb8d3e81099b8eb2e759
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-31 15:18:41 +0200

    Completato esercizio, creato progetto e inserito componente

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: 'BaseTitle',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    h1 {
+        color: rgb(255, 0, 0);
+        font-size: 60px;
+    }
+</style>
